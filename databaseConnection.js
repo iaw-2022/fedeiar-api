b9b547b4bfd9f75a0ac667b9790f7f55c2a2e79b
Federico <iarlorifederico@gmail.com>
2022-05-05 10:22:12 -0300

    Update databaseConnection.js

--- databaseConnection.js
+++ databaseConnection.js
@@ -7,7 +7,7 @@ const pool = new Pool({
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
-    password: process.env.DB_PASS
+    password: process.env.DB_PASSWORD
 });
 
 module.exports = pool;
