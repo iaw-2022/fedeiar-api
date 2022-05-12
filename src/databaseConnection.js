2830288fcd55cd814d594274d06d08c9f7aa971f
unknown <iarlorifederico@gmail.com>
2022-05-12 01:33:40 -0300

    prueba para que ande en Heroku

--- src/databaseConnection.js
+++ src/databaseConnection.js
@@ -2,13 +2,29 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const { Pool } = require('pg');
-const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    ssl: false
-});
+
+let pool;
+
+if(process.env.NODE_ENV !== 'production'){
+    pool = new Pool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        ssl: false
+    });
+} else{
+    pool = new Pool({
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        database: process.env.DB_DATABASE,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        ssl: { rejectUnauthorized: false }
+    });
+}
+
+
 
 module.exports = pool;
